b684a275745ee5ba651a10b250235faa287ce454
mishra_abhi <abhi200108010@gmail.com>
2022-08-18 23:26:05 +0530

    Added method to check user-status while opening app

--- app/src/main/java/com/example/clickme/MainActivity.java
+++ app/src/main/java/com/example/clickme/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.clickme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.clickme.auth.SignInActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    Boolean counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //========================================================================================
+    // Below code is related to phone authentication part
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkUserStatus();
+    }
+
+    private void checkUserStatus() {
+        SharedPreferences sharedPreferences=getSharedPreferences("logindata",MODE_PRIVATE);
+        counter = sharedPreferences.getBoolean("logincounter",Boolean.valueOf(String.valueOf(MODE_PRIVATE)));
+        if(!counter){
+            startActivity(new Intent(MainActivity.this, SignInActivity.class));
+            finish();
+        } else {
+            checkUserInfoStatus();
+        }
+    }
+
+    private void checkUserInfoStatus() {
+        SharedPreferences getUserInfoStatus = getSharedPreferences("userInfo", MODE_PRIVATE);
+        boolean userInfoSaved = getUserInfoStatus.getBoolean("writtenToDB", false);
+        if (!userInfoSaved) {
+            startActivity(new Intent(this, UserInfoActivity.class));
+            finish();
+        }
+    }
+
 }
