04809b6d15e052e504cabcd09d85957935c95d75
mishra_abhi <abhi200108010@gmail.com>
2022-08-28 23:31:49 +0530

    done admin fragment and AddNewPost activity

---
+++ app/src/main/java/com/example/clickme/AddNewPostActivity.java
@@ -0,0 +1,196 @@
+package com.example.clickme;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.clickme.models.Member;
+import com.example.clickme.models.Post;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class AddNewPostActivity extends AppCompatActivity {
+
+    private EditText captionEditText;
+    private ImageView postImage;
+    private Button addImageBtn, createPostBtn;
+    private ProgressBar progressBar;
+    private String caption;
+    private PostViewModel postViewModel;
+    private ActivityResultLauncher<String> postPhotoPicker;
+    Uri downloadUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_post);
+
+        captionEditText = findViewById(R.id.postEditText);
+        postImage = findViewById(R.id.postImage);
+        addImageBtn = findViewById(R.id.addPhotoInPost);
+        createPostBtn = findViewById(R.id.makePost);
+        postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
+        progressBar = findViewById(R.id.addingImage);
+        progressBar.setVisibility(View.GONE);
+
+        //Used for Edit Post
+        if (getIntent().getBooleanExtra("edit",false)) {
+            captionEditText.setText(getIntent().getStringExtra("caption"));
+            String intentUri = getIntent().getStringExtra("image");
+            Picasso.get().load(intentUri).into(postImage);
+        }
+
+
+        //This is new method to pick content from Storage.
+        //Alternate of startActivityForResult(DEPRECATED)
+        postPhotoPicker = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+//                            postImage.setImageURI(result);
+                        Picasso.get().load(result).into(postImage);
+                        progressBar.setVisibility(View.VISIBLE);
+                        createPostBtn.setEnabled(false);
+                        //this uri is of image. Push it to storage and get its link
+                        //in DB. Then display it.
+
+
+                        //See this must not be done now, Must do only when button create post clicked
+
+                        if (result != null) {
+                            StorageReference mStorageRef = FirebaseStorage.getInstance().getReference("Post_Pics");
+                            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                                    + "." + getFileExtension(result));
+
+                            UploadTask mUploadTask = fileReference.putFile(result);
+                            Task<Uri> urlTask = mUploadTask.continueWithTask(task -> {
+                                if (!task.isSuccessful()) {
+                                    throw Objects.requireNonNull(task.getException());
+                                }
+                                return fileReference.getDownloadUrl();
+                            }).addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    downloadUri = task.getResult();
+//                                    Toast.makeText(AddNewPostActivity.this, downloadUri.toString(), Toast.LENGTH_SHORT).show();
+
+                                    progressBar.setVisibility(View.GONE);
+                                    createPostBtn.setEnabled(true);
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(AddNewPostActivity.this, "No file selected", Toast.LENGTH_SHORT).show();
+
+                            progressBar.setVisibility(View.GONE);
+                            createPostBtn.setEnabled(true);
+                        }
+                    }
+
+//
+                });
+
+        addImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Here just launch above photoPicker
+                postPhotoPicker.launch("image/*");
+            }
+        });
+
+        createPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                caption = captionEditText.getText().toString();
+
+                SharedPreferences sharedPreferences = getSharedPreferences("logindata", MODE_PRIVATE);
+                String name = sharedPreferences.getString("name", "defName");
+//                String imageUrl = sharedPreferences.getString("imageUrl", "https://getodk-a3b1.kxcdn.com/uploads/default/d97aaae47866522c9404527cc25f6d6b11a0328c");
+                String location = sharedPreferences.getString("location", "defLocation");
+
+//                FirebaseAuth auth = FirebaseAuth.getInstance();
+//                FirebaseUser user = auth.getCurrentUser();
+//
+//                String name = user.getDisplayName();
+                String imageUrl = sharedPreferences.getString("imageUrl", null);
+                Member postCreator = new Member(name, imageUrl, location);
+
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss dd MMM, yyyy");
+                String currentDateTime = sdf.format(new Date());
+
+                String postImageUri = downloadUri == null ? null : downloadUri.toString();
+                Post post = post = new Post(caption, postImageUri, postCreator, currentDateTime, postCreator.getLocation());
+//                Toast.makeText(AddNewPostActivity.this, postImageUri, Toast.LENGTH_SHORT).show();
+                if (getIntent().getBooleanExtra("edit",false)) {
+                    //if image has not been edited then put original image back
+                    if (postImageUri == null)
+                        postImageUri = getIntent().getStringExtra("image");
+                    Picasso.get().load(postImageUri).into(postImage);
+                    postViewModel.editPost(getIntent().getStringExtra("uid"), caption, postImageUri);
+                }
+                else
+                    postViewModel.addPosts(post);
+                startActivity(new Intent(AddNewPostActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver CR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(CR.getType(uri));
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog alertDialog = discardOrKeepPost();
+        alertDialog.show();
+    }
+
+    private AlertDialog discardOrKeepPost() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Discard Post?")
+                .setMessage("Select an action to be performed!")
+                .setPositiveButton("Get Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(AddNewPostActivity.this, MainActivity.class));
+                        finish();
+                    }
+                })
+                .create();
+
+        return alertDialog;
+    }
+
+}
--- app/src/main/java/com/example/clickme/fragments/AdminFragment.java
+++ app/src/main/java/com/example/clickme/fragments/AdminFragment.java
@@ -1,16 +1,37 @@
 package com.example.clickme.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.clickme.AddNewPostActivity;
 import com.example.clickme.R;
+import com.example.clickme.adapters.PostsAdapter;
+import com.example.clickme.models.Member;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class AdminFragment extends Fragment {
+
+
+    private ArrayList<Member> adminList;
+    private RecyclerView recyclerView;
+    private PostsAdapter postsAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private FloatingActionButton addPostBtn;
+    SharedPreferences sharedPreferences;
+    private String phone;
 
     public AdminFragment() {
         // Required empty public constructor
@@ -24,7 +45,43 @@ public class AdminFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin, container, false);
+        View v = inflater.inflate(R.layout.fragment_admin, container, false);
+
+        sharedPreferences = this.getActivity().getSharedPreferences("logindata", Context.MODE_PRIVATE);
+        phone = sharedPreferences.getString("phone_num", "Number not found!");
+
+        addPostBtn = v.findViewById(R.id.addPost);
+
+        if(Objects.equals(phone, "8840849989") || Objects.equals(phone, "7052800709") || Objects.equals(phone, "1111111111")){
+            addPostBtn.setVisibility(View.VISIBLE);
+        }
+
+        addPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AddNewPostActivity.class));
+                finishThisFragmentAndActivity();
+            }
+        });
+
+
+        adminList = new ArrayList<Member>();
+
+        Member member = new Member("SEC_ADMIN_NAME", "Fasdfse", "Mumbai, Maharashtra");
+        adminList.add(member);
+        return v;
     }
+
+    private void finishThisFragmentAndActivity() {
+        getActivity().finish();
+    }
+
+//    private void setUpRecyclerView(View view) {
+//        recyclerView = view.findViewById(R.id.postRecyclerView);
+//        layoutManager = new LinearLayoutManager(getActivity());
+////        postsAdapter = new PostsAdapter(adminList); MemberAdapter use
+//
+//        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setAdapter(postsAdapter);
+//    }
 }
