83630e51e27c7edd4d9bd41c06ca04531d54f5eb
mishra_abhi <abhi200108010@gmail.com>
2022-08-29 00:00:18 +0530

    Done member and post fragment

--- app/src/main/java/com/example/clickme/fragments/MemberFragment.java
+++ app/src/main/java/com/example/clickme/fragments/MemberFragment.java
@@ -2,15 +2,34 @@ package com.example.clickme.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.clickme.MembersViewModel;
 import com.example.clickme.R;
+import com.example.clickme.adapters.MembersAdapter;
+import com.example.clickme.models.Member;
+
+import java.util.ArrayList;
 
 public class MemberFragment extends Fragment {
+
+    private ArrayList<Member> membersList;
+    private RecyclerView recyclerView;
+    private MembersAdapter membersAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private MembersViewModel membersViewModel;
+    private ProgressBar memberLoaderBar;
+    private SearchView searchView;
 
     public MemberFragment() {
         // Required empty public constructor
@@ -24,7 +43,58 @@ public class MemberFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_member, container, false);
+        membersList = new ArrayList<>();
+        View view = inflater.inflate(R.layout.fragment_member, container, false);
+        memberLoaderBar = view.findViewById(R.id.memberLoaderBar);
+
+        searchView = view.findViewById(R.id.searchView);
+        SearchText(searchView);
+
+        setRecyclerView(view);
+
+        membersViewModel = new ViewModelProvider(this).get(MembersViewModel.class);
+        membersViewModel.readMembers(membersList, membersAdapter, memberLoaderBar);
+        return view;
     }
+
+    private void setRecyclerView(View view) {
+
+        recyclerView = view.findViewById(R.id.memberRecyclerView);
+        layoutManager = new LinearLayoutManager(getActivity());
+        membersAdapter = new MembersAdapter(membersList);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(membersAdapter);
+    }
+
+    public void filterList(String text){
+        ArrayList<Member> filteredList = new ArrayList<>();
+        for (Member member: membersList) {
+            if(member.getName().toLowerCase().startsWith(text.toLowerCase())){
+                filteredList.add(member);
+            }
+        }
+
+        if(filteredList.isEmpty()){
+            Toast.makeText(getActivity(), "No data found", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            membersAdapter.setFilteredList(filteredList);
+        }
+    }
+
+    public void SearchText(SearchView searchView){
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/clickme/fragments/PostFragment.java
+++ app/src/main/java/com/example/clickme/fragments/PostFragment.java
@@ -3,16 +3,31 @@ package com.example.clickme.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import com.example.clickme.PostViewModel;
 import com.example.clickme.R;
+import com.example.clickme.adapters.PostsAdapter;
+import com.example.clickme.models.Post;
+
+import java.util.ArrayList;
 
 
 public class PostFragment extends Fragment {
 
+    private ArrayList<Post> postsList;
+    private RecyclerView recyclerView;
+    private PostsAdapter postsAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private PostViewModel postViewModel;
+    private ProgressBar postLoaderBar;
 
     public PostFragment() {
         // Required empty public constructor
@@ -26,7 +41,22 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        postsList = new ArrayList<Post>();
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        postLoaderBar = view.findViewById(R.id.postLoaderBar);
+        setUpRecyclerView(view);
+
+        postViewModel = new ViewModelProvider(this).get(PostViewModel.class);
+        postViewModel.readPosts(postsList, postsAdapter, postLoaderBar);
+        return view;
+    }
+
+    private void setUpRecyclerView(View view) {
+        recyclerView = view.findViewById(R.id.postRecyclerView);
+        layoutManager = new LinearLayoutManager(getActivity());
+        postsAdapter = new PostsAdapter(postsList, getContext());
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(postsAdapter);
     }
 }
